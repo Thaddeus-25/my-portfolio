4af9d0a265cf3a31d97cf8820e44108f965b7e96
Thaddeus Mak <thaddeusmak25@gmail.com>
2026-06-17 11:32:17 -1000

    Added files

---
+++ script.js
@@ -0,0 +1,20 @@
+// Smooth scrolling for navigation links
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener('click', function (e) {
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+
+    });
+
+});
+
+
+// Console message
+
+console.log("Portfolio website loaded successfully.");
